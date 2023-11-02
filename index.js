e39f0aa467427651389e899b92117d083dd0aecd
Gedion Adamu <gedionadamu@gmail.com>
2023-11-02 16:16:26 -0400

    created all cards and css

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 // import { CardTemplate } from "./card"
+// Creates a card that takes the num and suit of the playing card. 
+ const numArray=[1,2,3,4,5,6,7,8,9,"J","Q","K"]
+ const suitArray=["spades","clubs","diamonds","hearts"]
+const card = function CardTemplate(num, suit) {
 
- const card = function CardTemplate(num, suit) {
-    // const num = num;
-    // const suit = suit;
-   
-     
-    if(suit === "spades"){
+    if (suit === "spades") {
         suit = "&#x2664"
-     }
-    else if(suit === "clubs"){
-         suit = "&#x2667"
-     }
-    else if(suit === "diamonds"){
-         suit= "&#x2662"
-     }
-    else if (suit === "hearts"){
+    }
+    else if (suit === "clubs") {
+        suit = "&#x2667"
+    }
+    else if (suit === "diamonds") {
+        suit = "&#x2662"
+    }
+    else if (suit === "hearts") {
         suit = "&#x2661"
     }
     else
         console.log("not a vaild suit")
-     
 
-     const cardTemp= ` <div class="card_container">
-                     <div class= "card" >
-                         <p class="cardnum">${num}</p>
-                         <div class="suit">${suit}</div>
+
+    const cardTemp = ` <div class="card_container">
+                        <div class= "card ${suit}" >
+                            <p class="cardnum">${num}</p>
+                            <div class="suit">${suit}</div>
                             <div class="bigsuit">${suit}</div>
-                    </div >
-                 </div > `
-                  
-   
-                 const el = document.querySelector("#card")
-                 const filledcard = document.createElement("div")
-                 filledcard.innerHTML = cardTemp
-                 el.append(filledcard)
-                 
-}
+                         </div >
+                    </div > `
 
-card(2,"hearts")
+
+    const el = document.querySelector("#card")
+    const filledcard = document.createElement("div")
+    filledcard.innerHTML = cardTemp
+    el.append(filledcard)
+
+}
+ for(let i=0; i < numArray.length; i++){
+    for (let j=0; j<suitArray.length; j++){
+        card(numArray[i],suitArray[j])
+    }
+ }
+
+// card(2, "clubs")
