ab75fc36787a386cbb193b1cd890b80e0df2b75a
Gedion Adamu <gedionadamu@gmail.com>
2023-11-08 11:09:37 -0500

    Updated Card UI to show correct number of suits

--- javascript/card.js
+++ javascript/card.js
@@ -1,16 +1,34 @@
 function createCard(num, suit) {
-
+    let centerSuit= []
+    // console.log(num)
+    console.log(typeof num)
+    if (typeof num == "number"){
+    for(let i=0;i<num; i++){
+        centerSuit.push(`<div class="center-suits">${suit.code}</div>`)
+       
+    }}
+    else {
+        centerSuit.push(`<div class="center-suits">${suit.code}</div>`)
+    }
+    
+    
     const cardTemp = ` <div class="card-container">
                         <div class="card ${suit.name}" >
                             <div class="card-title">
                                 <p class="card-num">${num}</p>
                                 <div class="suit">${suit.code}</div>
                             </div>
-                            <div class="bigsuit">${suit.code}</div>
+                            <div class="center-suits-container">
+                            ${centerSuit}
+                            </div>
+                            <div class="card-title-bottom">
+                                <p class="card-num">${num}</p>
+                                <div class="suit">${suit.code}</div>
+                            </div>
                          </div >
                     </div > `
 
-
+                
     const el = document.querySelector("#card")
     const filledCard = document.createElement("div")
     filledCard.innerHTML = cardTemp
