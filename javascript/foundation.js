6f9998a7da8b4ed3c1fc118e8c0a7156a444ab37
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2023-11-10 18:34:47 +0400

    code cleanup

--- javascript/foundation.js
+++ javascript/foundation.js
@@ -1,33 +1,18 @@
 function createfoundationDeck() {
-    const suitArray = [
-        {
-            name: "spades",
-            code: "&#x2664"
-        }, {
-            name: "clubs",
-            code: "&#x2667"
-        }, {
-            name: "diamonds",
-            code: "&#x2662"
-        }, {
-            name: "hearts",
-            code: "&#x2661"
-        }
-    ];
 
-    const foundationCards = document.querySelectorAll(".foundation");
+    const foundationCards = document.querySelectorAll(".foundation")
     foundationCards.forEach(card => {
-        const suitsDisplay = document.createElement("div");
-        suitsDisplay.classList.add("suits-holder");
+        const suitsDisplay = document.createElement("div")
+        suitsDisplay.classList.add("suits-holder")
         suitArray.forEach(suit => {
-            const s = document.createElement("div");
-            s.classList.add("suit", suit.name);
-            s.innerHTML = suit.code;
-            suitsDisplay.append(s);
+            const s = document.createElement("div")
+            s.classList.add("suit", suit.name)
+            s.innerHTML = suit.code
+            suitsDisplay.append(s)
         })
 
-        card.append(suitsDisplay);
+        card.append(suitsDisplay)
     })
 
 }
-createfoundationDeck();
+createfoundationDeck()
