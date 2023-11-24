c8b98f91845ac28271ffbb8487565a00a1c1709d
Kassem Jenkins <kassemjenkins@gmail.com>
2023-11-24 06:06:45 -0800

    tableau update

--- javascript/tableau.js
+++ javascript/tableau.js
@@ -3,13 +3,13 @@ function createFoundationPiles() {
     const tableauCards = document.querySelectorAll(".tableau-pile");
    
     tableauCards.forEach((card, index) => {
-        let maxOffset = 336;
+        let maxOffset = 346;
         let cardId = card.id;
         let startingCount = Number(cardId.slice(-1)) - 1;
         for (let i = 0; i < startingCount; i++) {
             const flippedCard = document.createElement("div");
             flippedCard.classList.add("flipped");
-            const cardOffset = 351 + i * 15;
+            const cardOffset = 351 + i * 5;
             flippedCard.style.top = `${cardOffset}px`;
             card.appendChild(flippedCard);
             maxOffset = cardOffset
@@ -18,7 +18,7 @@ function createFoundationPiles() {
         const faceCard = createCard(numArray[index], suitArray[index % 4]);
         const innerCard = faceCard.children[0].children[0];
         faceCard.style.position = "absolute";
-        faceCard.style.top = `${maxOffset + 15}px`;
+        faceCard.style.top = `${maxOffset + 5}px`;
         innerCard.style.margin = "0px";
         card.appendChild(faceCard)
 
