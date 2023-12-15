d93c385b4ae3c29ef9bac260abd5a3bf610ca43b
Kassem Jenkins <kassemjenkins@gmail.com>
2023-12-15 05:58:59 -0800

    Randomization of cards at at start

--- javascript/index.js
+++ javascript/index.js
@@ -19,6 +19,22 @@ const suitArray = [
     },
 ]
 
+// Create a 2d array of the deck representing the suits and the numbers of the cards
+const cardDeckStatus = Array.from({ length: 4 }, () => Array(13).fill(0))
+
+// Create a holder for the initial tableau cards
+const tableauPiles = []
+
+// Create a holder for the initial stock cards
+const stockCards = []
+
+// "Enum" for the card piles
+const Piles = Object.freeze({
+    Foundation: "F",
+    Stock: "S",
+    Tableau: "T",
+})
+
 const stock = document.querySelector(".stock")
 const wasteContainer = document.querySelector(".waste-container")
 const childern = wasteContainer.children
@@ -26,15 +42,60 @@ const childern = wasteContainer.children
 function revealStock() {
     if (wasteContainer.children.length < 3) {
         for (let i = 0; i < 3; i++) {
-            createCard(numArray[i], suitArray[1])
+            let [num, suit] = stockCards[i]
+            createCard(numArray[num ?? i], suitArray[suit ?? 1])
         }
     } else if (wasteContainer.children.length >= 3) {
         while (wasteContainer.firstChild) {
             wasteContainer.removeChild(wasteContainer.firstChild)
         }
         for (let i = 0; i < 3; i++) {
-            createCard(numArray[i], suitArray[1])
+            let [num, suit] = stockCards[i]
+            createCard(numArray[num ?? i], suitArray[suit ?? 1])
         }
     }
 }
 stock.addEventListener("click", revealStock)
+
+// Maps the cards in the dack into the separate piles
+function mapDeckToPiles() {
+    // Instantiate the deck with placements for the tableau and stock cards
+    const cardsArray = Array(52)
+    cardsArray.fill(Piles.Stock, 0, 24)
+    cardsArray.fill(Piles.Tableau, 24)
+
+    shuffleCards(cardsArray)
+
+    for (let i = 0; i < cardsArray.length; i++) {
+        let cardNum = i % 13 //card numbers
+        let cardSuit = Math.floor(i / 13) // card suits
+        cardDeckStatus[cardSuit][cardNum] = cardsArray[i]
+
+        // Load the stock and tableau holders for the current game
+        if (cardsArray[i] === Piles.Stock) {
+            stockCards.push([cardNum, cardSuit])
+        } else if (cardsArray[i] === Piles.Tableau) {
+            tableauPiles.push([cardNum, cardSuit])
+        }
+    }
+
+    shuffleCards(stockCards)
+    shuffleCards(tableauPiles)
+}
+
+// "Shuffles" an array of cards
+function shuffleCards(array) {
+    // for (let i = 0; i < cardsArray.length; i++) {
+    //     // use Math.random to choose an element in the array after the current one
+    //     // and swap it with the current element
+    //     let j = Math.floor(Math.random() * (cardsArray.length - i - 1) + i)
+    //     [cardsArray[i], cardsArray[j]] = [cardsArray[j], cardsArray[i]]
+    // }
+
+    // Use Math.random to sort cards
+    array.sort(() => Math.random() - 0.5)
+
+    return array
+}
+
+mapDeckToPiles()
--- javascript/tableau.js
+++ javascript/tableau.js
@@ -6,6 +6,7 @@ function createTableauPiles() {
         const cardId = card.id
         const startingCount = Number(cardId.slice(-1)) - 1
         for (let i = 0; i < startingCount; i++) {
+            // Create the piles of cards that are initally flipped over
             const flippedCard = document.createElement("div")
             flippedCard.classList.add("card-back")
             const border = document.createElement("div")
@@ -17,8 +18,16 @@ function createTableauPiles() {
             maxOffset = cardOffset
         }
 
-        const faceCard = createCard(numArray[index], suitArray[index % 4])
+        // Use the card indexes from the tableau piles holder if it has cards
+        let faceCard
+        if (tableauPiles && tableauPiles.length > 0) {
+            let [num, suit] = tableauPiles[index]
+            faceCard = createCard(numArray[num], suitArray[suit])
+        } else {
+            faceCard = createCard(numArray[index], suitArray[index % 4])
+        }
         const innerCard = faceCard.children[0].children[0]
+        // Position the top cards over the flipped cards
         faceCard.style.position = "absolute"
         faceCard.style.top = `${maxOffset + 15}px`
         innerCard.style.margin = "0px"
