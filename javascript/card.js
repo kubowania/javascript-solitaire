c2682e16f64553fc760533f856139ea648450ff9
Joe Loera <joeloera7@gmail.com>
2024-08-09 07:26:25 -0700

    Updated card.js logic to include K,Q,J svgs, updated CSS for the svgs, and added SVG images

--- javascript/card.js
+++ javascript/card.js
@@ -1,10 +1,23 @@
 function createCard(num, suit) {
     let centerSuit = []
 
-    if (parseInt(num) !== NaN) {
+    function getFaceCardSvg(cardType, suitName) {
+        const svgs = {
+            K: `<img src="images/king-svgrepo-com.svg" class="face-card-svg ${suitName}" alt="King">`, // SVG for King
+            Q: `<img src="images/queen-svgrepo-com.svg" class="face-card-svg ${suitName}" alt="Queen">`, // SVG for Queen
+            J: `<img src="images/jack-svgrepo-com.svg" class="face-card-svg ${suitName}" alt="Jack">`, // SVG for Jack
+        }
+        return svgs[cardType] || ""
+    }
+
+    if (!isNaN(parseInt(num))) {
         for (let i = 0; i < num; i++) {
             centerSuit.push(`<div class="center-suits">${suit.code}</div>`)
         }
+    } else if (["K", "Q", "J"].includes(num)) {
+        centerSuit.push(
+            `<div class="center-suits">${getFaceCardSvg(num, suit.name)}</div>`
+        )
     } else {
         centerSuit.push(`<div class="center-suits">${suit.code}</div>`)
     }
@@ -12,20 +25,20 @@ function createCard(num, suit) {
     let joinedCenterSuites = centerSuit.join("")
 
     const cardTemp = ` <div class="card-container">
-                        <div class="card ${suit.name}" >
-                            <div class="card-title">
-                                <p class="card-num">${num}</p>
-                                <div class="suit">${suit.code}</div>
-                            </div>
-                            <div class="center-suits-container">
-                            ${joinedCenterSuites}
-                            </div>
-                            <div class="card-title-bottom">
-                                <p class="card-num">${num}</p>
-                                <div class="suit">${suit.code}</div>
-                            </div>
-                         </div >
-                    </div > `
+                            <div class="card ${suit.name}" >
+                                <div class="card-title">
+                                    <p class="card-num">${num}</p>
+                                    <div class="suit">${suit.code}</div>
+                                </div>
+                                <div class="center-suits-container">
+                                ${joinedCenterSuites}
+                                </div>
+                                 <div class="card-title-bottom">
+                                    <p class="card-num">${num}</p>
+                                    <div class="suit">${suit.code}</div>
+                                </div>
+                             </div >
+                        </div > `
 
     const el = document.querySelector(".waste-container")
     const filledCard = document.createElement("div")
